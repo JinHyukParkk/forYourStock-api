6033985a0170c9f3dbd99dae3dd2231e03ccdda9
hyuk <pjh08190819@naver.com>
2021-01-18 00:16:50 +0900

    Add log

--- main.go
+++ main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"forYourStock-api/router"
+	"os"
+	"io"
 )
 
 func main() {
+	 // Disable Console Color, you don't need console color when writing the logs to file.
+	 gin.DisableConsoleColor()
+
+	 // Logging to a file.
+	 f, _ := os.Create("log/gin.log")
+	 gin.DefaultWriter = io.MultiWriter(f)
+
 	router.SetRouter()
 
 	router.Router.Run(":8080")
