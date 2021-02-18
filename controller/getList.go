eabd4abf316ad7ef56cdb8b925820c0c4b2d65ea
hyuk <pjh08190819@naver.com>
2021-02-18 23:48:32 +0900

    change package

--- controller/getList.go
+++ controller/getList.go
@@ -1,18 +1,18 @@
 package controller
 
 import (
-	"forYourStock/api"
-	"os"
 	"fmt"
+	"forYourStock/controller/service"
+	"os"
 )
 
 func GetList() {
 	key := os.Getenv("APIKEY")
 
 	// 회사 리스트 list.zip 파일 불러오기
-	api.GetCompanyList(key)
+	service.GetCompanyList(key)
 
 	// 압출풀기
-	go api.GetList()
+	go service.GetList()
 	fmt.Println("Call GetList")
 }
