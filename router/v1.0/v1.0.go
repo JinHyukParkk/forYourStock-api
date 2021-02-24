7ac5afc0f0ea2a30a7459183d96968daf23ae8f8
hyuk <pjh08190819@naver.com>
2021-02-24 23:49:47 +0900

    API 구조 테스트

--- router/v1.0/v1.0.go
+++ router/v1.0/v1.0.go
@@ -1,18 +1,14 @@
 package routerv1
 
 import (
+	v1controller "forYourStock-api/controller/service/v1.0"
+
 	"github.com/gin-gonic/gin"
 )
-
-func ping(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-}
 
 func SetRouter(router *gin.RouterGroup) {
 	v1 := router.Group("/v1")
 	{
-		v1.GET("/ping", ping)
+		v1.GET("/ping", v1controller.Ping)
 	}
 }
